3f619ca1e812ef0e50a93ae31881049696f1a034
Raymon Epping <your@email.com>
2025-06-29 20:51:27 +0200

    29/06/2025 - Updated configuration and fixed bugs

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL || 'http://localhost:4000'
-    }
-  }
-})
+      apiBase: process.env.API_BASE_URL || "http://localhost:4000",
+    },
+  },
+});
